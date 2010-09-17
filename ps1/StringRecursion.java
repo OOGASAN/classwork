eba8dd7877f23bd255c00811b3b83a698953c198
sam <samtymorek@gmail.com>
2010-09-16 23:00:19 -0400

    adding recursion files

---
+++ ps1/StringRecursion.java
@@ -0,0 +1,17 @@
+public class StringRecursion {
+	public String stringToTest(String str) {
+		if (str == "" || str.length() == 1) {
+			return str;
+		} else {
+			char temp = str.charAt(0);
+			
+			// 
+		}	
+	}
+	
+	public static void main(String[] args) {
+		// TODO: get from input
+		String stringToTest = "Harvard";
+		System.out.println(printReverse(stringToTest));	
+	}	
+}
