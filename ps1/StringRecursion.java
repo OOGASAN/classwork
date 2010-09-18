adf89bf13df7b28964920f4b05df1517f0067daf
sam <samtymorek@gmail.com>
2010-09-18 12:50:21 -0400

    adding reverse string recurseive function

--- ps1/StringRecursion.java
+++ ps1/StringRecursion.java
@@ -1,17 +1,27 @@
+import java.util.*;
+
 public class StringRecursion {
-	public String stringToTest(String str) {
-		if (str == "" || str.length() == 1) {
-			return str;
-		} else {
-			char temp = str.charAt(0);
-			
-			// 
-		}	
-	}
-	
-	public static void main(String[] args) {
-		// TODO: get from input
-		String stringToTest = "Harvard";
-		System.out.println(printReverse(stringToTest));	
-	}	
+
+    public static String reverse(String str) {
+        if (str == null) {
+            return null;
+        } else if (str.length() == 0 || str.length() == 1) {
+            return str;
+        } else {
+            char temp = str.charAt(str.length()-1);
+            String sub = str.substring(0,str.length()-1);
+
+            return temp + reverse(sub);
+        } 
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string to reverse");
+        String stringToTest = in.nextLine();
+        System.out.println(reverse(stringToTest)); 
+        System.out.println("Handling null input:");        
+        String nulltest = null;
+        System.out.println(reverse(nulltest));
+    } 
 }
--- ps1/string_recursion.py
+++ ps1/string_recursion.py
@@ -1,55 +1,8 @@
-#def print_reverse(string):
-##	print string
-#	if string == "" or len(string) == 0:
-#		return string
-#	else:
-#		bkwds = ''
-#		for i in range(len(string)):
-#			bkwds += string[(len(string)-1) - i]
-#			
-##		print bkwds[1:]
-#		
-#		return print_reverse(bkwds[1:])
-##		return print_reverse(bkwds[:-1])
-#		
-#print print_reverse("harvard")
-
-
-def print_reverse(string):
-#	print string
-	if string == "" or len(string) == 0:
+def reverse(string):
+	if string == "" or len(string) == 1:
 		return string
 	else:
-#		print string
-		newstring = print_reverse(string[1])
-		print newstring
-		bkwds = ''
-		for i in range(len(newstring)):
-			bkwds += string[(len(newstring)-1) - i]
-			
-		print bkwds
 		
-		return bkwds
-#		return print_reverse(bkwds[:-1])
+		return string[-1] + print_reverse(string[:-1])
 		
-print print_reverse("harvard")
-
-#def print_reverse(string):
-#	if string == "" or len(string) == 0:
-#		return string
-#	else:
-#		print string
-#		outer_swapped = ''
-#		newstring = print_reverse(string[1:])
-#		if len(newstring) > 2:
-#			outer_swapped = newstring[-1]
-#			print outer_swapped
-#			outer_swapped += newstring[1:-1]
-#			print outer_swapped
-#			outer_swapped += newstring[0]
-#			outer_swapped = string
-#		
-#		return outer_swapped
-##		return print_reverse(bkwds[:-1])
-#		
-#print print_reverse("harvard")
+print reverse("yard")
