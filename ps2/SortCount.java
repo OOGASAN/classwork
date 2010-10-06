72717d68f8cd46e277f6559adc3096dd21e312a9
stymo <samtymorek@gmail.com>
2010-10-05 21:55:01 -0400

    temporarily changing the max value for testing

--- ps2/SortCount.java
+++ ps2/SortCount.java
@@ -22,7 +22,7 @@ public class SortCount {
      * the integers in the test arrays are drawn from the range 
      * 0, ..., MAX_VAL 
      */
-    private static int MAX_VAL = 65536;
+    private static int MAX_VAL = 20;
 
     private static long compares;     // total number of comparisons
     private static long moves;        // total number of moves
